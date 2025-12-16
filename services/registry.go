62a4e17d1dc0fd9771293025da06c016dc44e74d
taufiqalfianto <taufikalfianto8@gmail.com>
2025-12-16 10:29:59 +0700

    feat: add service registry for user service management

--- services/registry.go
+++ services/registry.go
@@ -1,1 +1,24 @@
 package services
+
+import (
+	"user-service/repositories"
+	services "user-service/services/user"
+)
+
+type Registry struct {
+	repositories repositories.IRepositoryRegistry
+}
+
+type IServiceRegistry interface {
+	GetUser() services.IUserService
+}
+
+func NewServiceRegistry(repositories repositories.IRepositoryRegistry) IServiceRegistry {
+	return &Registry{
+		repositories: repositories,
+	}
+}
+
+func (r *Registry) GetUser() services.IUserService {
+	return services.NewUserService(r.repositories)
+}
